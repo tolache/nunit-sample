64f9a4b79c7daded6f9373e4cb429139cc38946e
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-08-16 17:59:55 +0200

    update Helper.cs

--- TestHelper/Helper.cs
+++ TestHelper/Helper.cs
@@ -7,7 +7,7 @@ public static class Helper
     public static void IncreaseTestDuration()
     {
         int testDurationSec = 10; 
-        TestContext.Out.WriteLine($"Falling asleep for {testDurationSec}...");
+        TestContext.Out.WriteLine($"Falling asleep for {testDurationSec} seconds...");
         Thread.Sleep(testDurationSec * 1000);
         TestContext.Out.WriteLine("Woken up.");
     }
