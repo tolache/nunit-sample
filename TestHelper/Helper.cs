b36e815acb0c245f499777fbe657f525c2414f6e
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-10-11 15:41:15 +0200

    upgrade TagetFramework to net7.0

--- TestHelper/Helper.cs
+++ TestHelper/Helper.cs
@@ -6,7 +6,7 @@ public static class Helper
 {
     public static void IncreaseTestDuration()
     {
-        int testDurationSec = 10; 
+        int testDurationSec = 1; 
         TestContext.Out.WriteLine($"Falling asleep for {testDurationSec} seconds...");
         Thread.Sleep(testDurationSec * 1000);
         TestContext.Out.WriteLine("Woken up.");
