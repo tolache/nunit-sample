0425d02ea01ca4eec3d753db73023d076c0a17bb
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-11-08 18:32:39 +0100

    add dummy tests with 10s duration

---
+++ TestClass1.cs
@@ -0,0 +1,53 @@
+namespace nunit_sample;
+
+[TestFixture]
+public class TestClass1
+{
+    private string _className;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _className = GetType().Name;
+    }
+
+    [Test]
+    public void Test1()
+    {
+        string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+        string testName = $"{_className}.{methodName}";
+        TestDelayer.Delay();
+        TestContext.Out.WriteLine($"{testName} completed.");
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+        string testName = $"{_className}.{methodName}";
+        TestDelayer.Delay();
+        TestContext.Out.WriteLine($"{testName} completed.");
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+        string testName = $"{_className}.{methodName}";
+        TestDelayer.Delay();
+        TestContext.Out.WriteLine($"{testName} completed.");
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+        string testName = $"{_className}.{methodName}";
+        TestDelayer.Delay();
+        TestContext.Out.WriteLine($"{testName} completed.");
+        Assert.Pass();
+    }
+}
---
+++ TestDelayer.cs
@@ -0,0 +1,13 @@
+﻿namespace nunit_sample;
+
+public static class TestDelayer
+{
+    private const double TestDurationS = 10;
+    
+    private static int TestDurationMs => Convert.ToInt32(TestDurationS * 1000);
+
+    public static void Delay()
+    {
+        Thread.Sleep(TestDurationMs);
+    }
+}
